46b25689379a634b197a3d92e87540a447c2a201
tankvaishali <vaishalitank28603@gmail.com>
2025-03-07 09:42:13 +0530

    change card in show jsx

--- src/Components/Product/Show.jsx
+++ src/Components/Product/Show.jsx
@@ -1,14 +1,7 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { Box, Button } from '@mui/material';
+import { Box, Card, CardActions, CardContent, CardMedia, IconButton, styled, Typography } from '@mui/material';
 import { BsWhatsapp } from 'react-icons/bs';
-import { AiFillProduct, AiOutlineEye, AiOutlineShoppingCart } from 'react-icons/ai';
+
 import ButtonCom from '../HOC/ButtonCom';
 
 const ProductCard = styled(Card)(({ theme }) => ({
