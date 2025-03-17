28c9576d58d8be64b7150e9d22af2fdc56179f20
surbhi <surbhipansuriya633@gmail.com>
2025-03-17 14:46:55 +0530

    set lending section in all components

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import HOC from '../HOC/Hoc'
 import "../../Assets/css/Contact.css";
 import ConatactForm from './ConatactForm'
+import Lendingsec from '../Lendingsec';
 
 function Contact() {
   return (
     <>
+      <Lendingsec Page={'Contact'} />
       <ConatactForm />
     </>
   )
--- src/Components/HowTo/Howto.jsx
+++ src/Components/HowTo/Howto.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import HOC from '../HOC/Hoc'
 import "../../Assets/css/Howto.css";
 import FollowStep from './FollowStep'
+import Lendingsec from '../Lendingsec';
 
 function Howto() {
   return (
     <>
+      <Lendingsec Page={'Howto'} />
       <FollowStep />
     </>
   )
